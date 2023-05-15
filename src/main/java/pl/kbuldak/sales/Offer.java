0188278c95cbd3acd0929232402659e623223df2
karlbuldak <karolbuldak@gmail.com>
2023-05-15 19:55:02 +0200

    pp4 zajecia

---
+++ src/main/java/pl/kbuldak/sales/Offer.java
@@ -0,0 +1,7 @@
+package pl.kbuldak.sales;
+
+import java.math.BigDecimal;
+
+public class Offer {
+    BigDecimal a;
+}
--- src/test/java/pl/kbuldak/sales/CollectingProductsTest.java
+++ src/test/java/pl/kbuldak/sales/CollectingProductsTest.java
@@ -1,37 +1,37 @@
-package pl.kbuldak.sales;
-
-import org.junit.jupiter.api.Test;
-
-import java.util.UUID;
-
-public class CollectingProductsTest {
-    @Test
-    void itAllowsToAddProduct() {
-        //ARRANGE
-        Sales sales = thereIsSalesModule();
-        String product1 = thereIsProduct();
-        String customerId = thereIsCustomer("Kuba");
-
-        //Act
-        sales.addToCart(customerId, product1);
-
-        //Assert
-        assertThereIsXProductsInCustomerCart(1, customerId);
-    }
-
-    private void assertThereIsXProductsInCustomerCart(int totalProductsQuantity, String customerId) {
-
-    }
-
-    private String thereIsCustomer(String id) {
-        return id;
-    }
-
-    private String thereIsProduct() {
-        return UUID.randomUUID().toString();
-    }
-
-    private Sales thereIsSalesModule() {
-        return new Sales();
-    }
-}
+//package pl.kbuldak.sales;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import java.util.UUID;
+//
+//public class CollectingProductsTest {
+//    @Test
+//    void itAllowsToAddProduct() {
+//        //ARRANGE
+//        Sales sales = thereIsSalesModule();
+//        String product1 = thereIsProduct();
+//        String customerId = thereIsCustomer("Kuba");
+//
+//        //Act
+//        sales.addToCart(customerId, product1);
+//
+//        //Assert
+//        assertThereIsXProductsInCustomerCart(1, customerId);
+//    }
+//
+//    private void assertThereIsXProductsInCustomerCart(int totalProductsQuantity, String customerId) {
+//
+//    }
+//
+//    private String thereIsCustomer(String id) {
+//        return id;
+//    }
+//
+//    private String thereIsProduct() {
+//        return UUID.randomUUID().toString();
+//    }
+//
+//    private Sales thereIsSalesModule() {
+//        return new Sales();
+//    }
+//}
